456c65e0a625629e5d970a539ca32c551b8c7514
Mcwad <marcuswadsackett@gmail.com>
2025-01-19 16:37:31 +0100

    cleanup

--- src/Model/GenerateBlocks.java
+++ src/Model/GenerateBlocks.java
@@ -5,26 +5,33 @@ package Model;
 
 import java.util.ArrayList;
 
-//generates blocks randomly based on current level
 public class GenerateBlocks {
     public static ArrayList<Block> generateBlocks(int level) {
         double rows = 4 + Math.floor(level/5.0);
         double columns = 5 + Math.floor(level/5.0);
         ArrayList<Block> blockList = new ArrayList<>();
+
         //spacing between blocks
         double space = Model.OptionsModel.getSceneWidth()/Math.sqrt(columns)/Math.sqrt(rows)/40;
         double blockWidth = Model.OptionsModel.getSceneWidth()/columns-space;
         double blockHeight = Model.OptionsModel.getSceneHeight()/3/rows-space;
 
         //generates probabilities for each row, so the top rows have the most powerful blocks
+        //each row has a probabilities-array associated with it
+        //each index in a probabilities-array represents the chance for each tier
         double[][] probabilities = new double[(int) rows][Block.getMaxTier()];
         for (int i = 0; i < probabilities.length; i++) {
+            // level makes the max tier only go up to current level, eg, level 1 can only generate tier 1
+            // Math.sqrt(level/5) makes the tiers of blocks scale with levels, eg higher level generates (mostly) higher tiers
+            // 5+i/rows*10 makes the tiers of blocks scale with rows
+            // 0.9 determines the spread
             probabilities[probabilities.length-1-i] = generateProbabilities(level, Math.sqrt(level/5)-5+i/rows*10, 0.9);
         }
         //generates the blocks based on the probabilities
         for (double i = 0; i < columns; i++) {
             for (double j = 0; j < rows; j++) {
-                //generates empty spaces randomly
+                //generates empty spaces randomly, top rows have low amount of spaces
+                //while bottom rows have high amount of spaces
                 if (Math.random() > Math.exp(-Math.pow(j, 2)/150/Math.pow(level, 0.25)*10/rows)) {
                     continue;
                 }
@@ -36,7 +43,7 @@ public class GenerateBlocks {
         return blockList;
     }
 
-    //generates an array where each index represents the probability for a tier
+    //generates an array where each index represents the probability of a tier
     public static double[] generateProbabilities(int level, double mean, double spread) {
         double[] probabilities = new double[level];
         double sum = 0;
